b79749fc5f924189abe333f4f66149a46560f40e
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-02-24 16:55:44 +0100

    added capture time to camera interface

--- include/camera_interface.h
+++ include/camera_interface.h
@@ -1,6 +1,7 @@
 #ifndef CAMERA_INTERFACE_H
 #define CAMERA_INTERFACE_H
 
+#include <chrono>
 #include <opencv2/core/core.hpp>
 
 class CameraInterface
@@ -13,7 +14,8 @@ public:
 	
 	virtual bool isOpened() = 0;
 	virtual bool read(cv::Mat &img) = 0;
-
+    virtual double captureTime();
+    
     virtual int getWidth() = 0;
     virtual int getHeight() = 0;
 
