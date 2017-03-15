d090e4f64f792769e15b2fd0c38f35f08f5929b9
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-03-15 19:16:12 +0100

    make capturing of background an explicit function

--- examples/detector_test.cpp
+++ examples/detector_test.cpp
@@ -7,20 +7,6 @@ int main(void) {
     cam.calibrate("../config/see3cam.yml");
     cam.start();
 
-    // capture and save background
-    cv::Mat frame, background;
-    cam.read(frame);
-    background = cv::Mat(frame.size(), CV_32FC3, cv::Scalar(0,0,0));
-    for (int i=0; i<50; i++) {
-        cam.read(frame);
-        cv::accumulate(frame, background);
-    }
-    background /= 50;
-    background.convertTo(background, CV_8UC3);
-    cv::imwrite("background.png", background);
-    imshow("image", background);
-    cv::waitKey(2000);
-
     // create detector
     Detector detector("../config/detector.yml", "background.png");
 
