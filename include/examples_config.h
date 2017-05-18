1304ed4c669cd89118d216ea2788aa20315ff00d
Maarten Verbandt <maarten.verbandt@hotmail.com>
2017-05-18 22:31:07 +0200

    Added function to catch keyboard enter

--- include/examples_config.h
+++ include/examples_config.h
@@ -7,4 +7,21 @@
 //#define EXAMPLE_COMMUNICATOR_INTERFACE "enp0s25"
 #define EXAMPLE_COMMUNICATOR_INTERFACE "wlp12s0"
 
+// Break from while loop: should move to separate file..
+#include <termios.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int kbhit()
+{
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
 #endif //EXAMPLES_CONFIG_H
