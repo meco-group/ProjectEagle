50ba83c0c81b08511a1c9997526f2dbc86928e70
PeterCoppens <petercoppens@skynet.be>
2017-07-10 13:35:32 +0200

    fixed arg error for experiments

--- spike/experiments.cpp
+++ spike/experiments.cpp
@@ -2,11 +2,14 @@
 #include "experiments.h"
 
 int main(int argc, char* argv[]) {
+    string str = "transmit";
     // Parse arguments
     const int mode = argc > 1 ? atoi(argv[1]) : 0;
     const string config1 = argc > 2 ? argv[2] : "../config/ceil1_cam.xml";
     const string config2 = argc > 3 ? argv[3] : "../config/ceil2_cam.xml";
-    const bool transmit = argc > 4 ? argv[4] == "transmit" : false;
+    const bool transmit = argc > 3 ? (!str.compare(argv[3])) : false;
+
+    cout << str << " " << transmit << '\n';
     switch (mode) {
         case 0:
             cal_test(config1);
