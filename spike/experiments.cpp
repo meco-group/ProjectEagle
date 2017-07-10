b2a188421c095f15d679835ba47f0d46324a841f
PeterCoppens <petercoppens@skynet.be>
2017-07-10 10:31:08 +0200

    woops

--- spike/experiments.cpp
+++ spike/experiments.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char* argv[]) {
     // Parse arguments
     const int mode = argc > 1 ? atoi(argv[1]) : 2;
-    const string config1 = argc > 2 ? argv[1] : "../config/ceil1_cam.xml";
-    const string config2 = argc > 3 ? argv[1] : "../config/ceil2_cam.xml";
+    const string config1 = argc > 2 ? argv[2] : "../config/ceil1_cam.xml";
+    const string config2 = argc > 3 ? argv[3] : "../config/ceil2_cam.xml";
     switch (mode) {
         case 0:
             cal_test(config1);
