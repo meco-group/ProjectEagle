7d0b194d39925921a5ae509f41e2eae72fa804fd
Johan Philips <johan.philips@kuleuven.be>
2017-07-10 14:09:24 +0200

    some pi adjustments

--- include/examples_config.h
+++ include/examples_config.h
@@ -6,8 +6,8 @@
 #define EXAMPLE_CAMERA_CALIBRATION "../config/see3cam.yml"
 //#define EXAMPLE_COMMUNICATOR_INTERFACE "enp0s25"
 //#define EXAMPLE_COMMUNICATOR_INTERFACE "wlp12s0"
-#define EXAMPLE_COMMUNICATOR_INTERFACE "wlp58s0"
-//#define EXAMPLE_COMMUNICATOR_INTERFACE "wlan0"
+//#define EXAMPLE_COMMUNICATOR_INTERFACE "wlp58s0"
+#define EXAMPLE_COMMUNICATOR_INTERFACE "wlan0"
 
 // Break from while loop: should move to separate file..
 #include <termios.h>
