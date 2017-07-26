01e88913418191417ba85445622a2e197c96a3c4
PeterCoppens <petercoppens@skynet.be>
2017-07-26 11:18:29 +0200

    working on resolution in config

--- src/image_transmitter/src/image_transmitter.cpp
+++ src/image_transmitter/src/image_transmitter.cpp
@@ -20,6 +20,7 @@ int main(int argc, char* argv[]) {
     V4L2Camera *cam = getCamera(cameraSettings.camIndex, cameraSettings.camType);
 
     // Start camera
+    cam->setResolution(cameraSettings.res_width, cameraSettings.res_height);
     // cam->calibrate(cameraSettings.calPath); //camera can be calibrated
     cam->start();
 
