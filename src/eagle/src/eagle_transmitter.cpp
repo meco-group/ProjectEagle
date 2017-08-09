f7acbe8fe4d914755f8f2914449af7fb4428e835
rubenvanparys <ruben.vanparys@kuleuven.be>
2017-08-09 19:36:38 +0200

    cleanup and fix? in eagle_transmitter

--- src/eagle/src/eagle_transmitter.cpp
+++ src/eagle/src/eagle_transmitter.cpp
@@ -18,18 +18,17 @@ int main(int argc, char* argv[]) {
     double frequency = std::stod(frequency_str);
     bool image_stream = (image_stream_str == "1");
 
-    // Open settings file
+    // open settings file
     CameraSettings cameraSettings;
     cameraSettings.read(config);
     ComSettings comSettings;
     comSettings.read(config);
 
-    // EXAMPLE_CAMERA_T cam(EXAMPLE_CAMERA_INDEX);
     V4L2Camera *cam = getCamera(cameraSettings.camIndex, cameraSettings.camType);
 
-    // Start camera
+    // start camera
     cam->setResolution(cameraSettings.res_width, cameraSettings.res_height);
-    cam->calibrate(cameraSettings.calPath); //camera can be calibrated
+    cam->calibrate(cameraSettings.calPath);
     cam->start();
 
     // setup the communication
@@ -49,16 +48,14 @@ int main(int argc, char* argv[]) {
     // create detector
     Detector detector(cameraSettings.detPath, cameraSettings.bgPath);
 
-    // start detecting
-    cv::Mat im;
-
+    Mat im;
     Mat R; Mat T;
     cv::FileStorage fs(cameraSettings.extPath, cv::FileStorage::READ);
     fs["rotation_matrix"] >> R;
     fs["translation_matrix"] >> T;
     fs.release();
 
-    // Make a robot which the camera should find
+    // make robots which the camera should find
     Robot dave(0, 0.55, 0.4, cv::Scalar(17, 110, 138), T, R);
     Robot krist(1, 0.55, 0.4, cv::Scalar(138, 31, 17), T, R);
     Robot kurt(9, 0.55, 0.4, cv::Scalar(17, 138, 19), T, R);
@@ -70,9 +67,10 @@ int main(int argc, char* argv[]) {
     std::cout << "T: \n" << T << "\n";
 
     // main execution loop
-    int nof = 0;
     int dt = 1000/frequency;
     auto t0 = std::chrono::high_resolution_clock::now();
+    double capture_time;
+    double capture_time2;
     while ( !io::kbhit() ) {
         //  check time
         auto t = std::chrono::high_resolution_clock::now();
@@ -80,31 +78,31 @@ int main(int argc, char* argv[]) {
             continue;
         }
         t0 = t;
-        // Detect the robots/obstacles
-        cam->read(im); nof++;
+        // capturer image
+        cam->read(im);
+        capture_time = cam->captureTime();
+        capture_time2 = cam->getV4L2CaptureTime();
+        std::cout << "[" << capture_time << " vs " << capture_time2 << "]" << std::endl;
+        // detect the robots/obstacles
         detector.search(im, robots, obstacles);
-
-        // Send information to interested listeners
-        int i = 0;
-        int k = 0;
 
         // pack robots
         size_t n_detected_robots = 0;
         eagle::header_t mheaders[robots.size()];
         eagle::marker_t markers[robots.size()];
-        for (k = 0; k < robots.size(); k++) {
+        for (int k = 0; k < robots.size(); k++) {
             if( robots[k]->detected() ) {
                 mheaders[n_detected_robots].id = eagle::MARKER;
-                mheaders[n_detected_robots].time = 0;
+                mheaders[n_detected_robots].time = capture_time;
                 markers[n_detected_robots++] = robots[k]->serialize();
             }
         }
         // pack obstacles
         eagle::header_t oheaders[obstacles.size()];
         eagle::obstacle_t obs[obstacles.size()];
-        for (k = 0; k < obstacles.size(); k++) {
+        for (int k = 0; k < obstacles.size(); k++) {
             oheaders[k].id = eagle::OBSTACLE;
-            oheaders[k].time = 0;
+            oheaders[k].time = capture_time;
             obs[k] = obstacles[k]->serialize();
         }
 
@@ -112,17 +110,18 @@ int main(int argc, char* argv[]) {
         std::vector< size_t > sizes = std::vector< size_t >(2*n_detected_robots + 2*obstacles.size(),0);
         std::vector< const void* > data = std::vector< const void* >(sizes.size());
 
-        for (k = 0; k < n_detected_robots; k++) {
-            sizes[i] = sizeof(eagle::header_t);
+        int i = 0;
+        for (int k = 0; k < n_detected_robots; k++) {
+            sizes[i++] = sizeof(eagle::header_t);
             data[i++] = (const void*)(&mheaders[k]);
-            sizes[i] = sizeof(eagle::marker_t);
+            sizes[i++] = sizeof(eagle::marker_t);
             data[i++] = (const void*)(&markers[k]);
         }
 
-        for (k = 0; k < obstacles.size(); k++) {
-            sizes[i] = sizeof(eagle::header_t);
+        for (int k = 0; k < obstacles.size(); k++) {
+            sizes[i++] = sizeof(eagle::header_t);
             data[i++] = (const void*)(&oheaders[k]);
-            sizes[i] = sizeof(eagle::obstacle_t);
+            sizes[i++] = sizeof(eagle::obstacle_t);
             data[i++] = (const void*)(&obs[k]);
         }
 
@@ -132,7 +131,7 @@ int main(int argc, char* argv[]) {
         if (image_stream) {
             // draw everything to give some feedback
             detector.draw(im, robots, obstacles);
-            imheader.time = k;
+            imheader.time = capture_time;
             cv::imencode(".jpg", im, buffer, compression_params);
             com.shout(&imheader, buffer.data(), sizeof(imheader), buffer.size(), comSettings.group);
         }
