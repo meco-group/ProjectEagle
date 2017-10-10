d7a091238252fb41b72b4c9be49e0849b8c718bf
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-10-10 14:39:07 +0200

    Make calibrate and stereo calibrate pure standalone

--- eagle/src/exec/calibrate.cpp
+++ eagle/src/exec/calibrate.cpp
@@ -1,14 +1,68 @@
-#include "calibrator.h"
-#include "utils.h"
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <opencv/cv.hpp>
+
+#include <pattern_extractor.h>
+#include <utils.h>
+#include <config_helper.h>
 
 using namespace eagle;
+
+/*
+ * Compute the internal calibration matrix together with the distortion vector. 
+ * These are saved in the provided config file.
+ */
 
 int main(int argc, char* argv[]) {
     std::string config_path = (argc > 1) ? argv[1] : CONFIG_PATH;
     std::string images_path = (argc > 2) ? argv[2] : CAL_IMAGES_PATH;
-    // calibrate
-    Calibrator cal(config_path);
-    cal.execute(images_path,false);
-    cal.save(config_path);
-    return 0;
-}
+    
+    // extract pattern from images
+    PatternExtractor extractor(config_path);
+    std::vector<cloud2_t> image_pnts = extractor.extract(images_path, false);
+    std::vector<std::vector<cv::Point3f>> pattern_pnts = extractor.pattern().reference(image_pnts.size());
+
+    // Read config file
+    cv::FileStorage fs(config_path, cv::FileStorage::READ);
+    int flag = 0;
+    if (((int)fs["calibrator"]["calib_fix_principal_point"] == 1)) flag |= CV_CALIB_FIX_PRINCIPAL_POINT;
+    if (((int)fs["calibrator"]["calib_zero_tangent_dist"] == 1)) flag |= CV_CALIB_ZERO_TANGENT_DIST;
+    if (((int)fs["calibrator"]["calib_fix_aspect_ratio"] == 1)) flag |= CV_CALIB_FIX_ASPECT_RATIO;
+    int width = fs["camera"]["resolution"]["width"];
+    int height = fs["camera"]["resolution"]["height"];
+    cv::Size size(width,height);
+    fs.release();
+
+    // initialize camera_matrix & distortion_vector
+    cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
+    if (flag & CV_CALIB_FIX_ASPECT_RATIO) {
+        camera_matrix.at<double>(0, 0) = 1.0;
+    }
+    cv::Mat distortion_vector = cv::Mat::zeros(8, 1, CV_64F);
+
+    // find intrinsic and extrinsic camera parameters
+    cv::Mat rvecs, tvecs;
+    double reproj_error = cv::calibrateCamera(pattern_pnts, image_pnts, size, camera_matrix,
+        distortion_vector, rvecs, tvecs, flag|CV_CALIB_FIX_K4|CV_CALIB_FIX_K5);
+    std::cout << "Calibration rms error: " << reproj_error << std::endl;
+    if(reproj_error > 2) {
+        std::cout << "Reprojection error is too high to consider the camera calibration succesful.." << std::endl << "Calibration failed." << std::endl;
+        return -1;
+    }
+    if(!(cv::checkRange(camera_matrix) && cv::checkRange(distortion_vector))){
+        std::cout << "The camera_matrix or distortion vector were out of range." << std::endl << "Calibration failed." << std::endl;
+        return -1;
+    }
+
+    // print the parameters
+    std::cout << "camera matrix " << camera_matrix << std::endl;
+    std::cout << "distortion vector " << distortion_vector << std::endl;
+    // save parameters
+    std::map<std::string, cv::Mat> matrices;
+    matrices["camera_matrix"] = camera_matrix;
+    matrices["distortion_vector"] = distortion_vector;
+    dump_matrices(config_path, matrices);
+    set_calibrated(config_path, true);
+    std::cout << "Saved calibration information succesfully." << std::endl;
+};
--- eagle/src/exec/stereo_calibrate.cpp
+++ eagle/src/exec/stereo_calibrate.cpp
@@ -1,8 +1,19 @@
-#include "calibrator.h"
-#include "camera.h"
-#include "utils.h"
+#include <iostream>
+#include <fstream>
+#include <camera.h>
+
+#include <pnp_pattern_extractor3.h>
+#include <planar_pattern_extractor3.h>
+#include <utils.h>
+#include <config_helper.h>
 
 using namespace eagle;
+
+/*
+ * Compute the transform between the calibrated camera pair Camera1-Camera2
+ * The transform is so that camera1 is the base and camera2 is expressed in the frame of camera1
+ * p1 = R*p2+t
+ */
 
 int main(int argc, char* argv[]) {
     std::string config_path1 = (argc > 1) ? argv[1] : CONFIG_PATH;
@@ -10,54 +21,94 @@ int main(int argc, char* argv[]) {
     std::string images_path1 = (argc > 3) ? argv[3] : CAL_IMAGES_PATH;
     std::string images_path2 = (argc > 4) ? argv[4] : CAL_IMAGES_PATH;
 
-    // calibrate the first camera
-    Calibrator cal1(config_path1);
-    cal1.execute(images_path1);
-    // cal1.save(config_path1);
+    // read the data images and extract 3d point cloud
+    PatternExtractor3 *extractor1 = new PnpPatternExtractor3(config_path1);
+    extractor1->set_skip_invalid(false);
+    std::vector<cloud3_t> points1 = extractor1->extractpath(images_path1);
+    PatternExtractor3 *extractor2 = new PnpPatternExtractor3(config_path2);
+    extractor2->set_skip_invalid(false);
+    std::vector<cloud3_t> points2 = extractor2->extractpath(images_path2);
 
-    // calibrate the second camera
-    Calibrator cal2(config_path1);
-    cal2.execute(images_path2);
-    // cal2.save(config_path2);
+    std::ofstream outputFile1("points1.txt");
+    std::ofstream outputFile2("points2.txt");
 
-    // TODO we could get cameraMatrix, distCoeffs from cal file (from intrinsic calibration)
-    cv::FileStorage fs1(config_path1, cv::FileStorage::READ);
-    cv::FileStorage fs2(config_path2, cv::FileStorage::READ);
-    cv::Mat K1, K2, D1, D2;
-    fs1["camera"]["camera_matrix"] >> K1;
-    fs1["camera"]["distortion_vector"] >> D1;
-    fs2["camera"]["camera_matrix"] >> K2;
-    fs2["camera"]["distortion_vector"] >> D2;
-    fs2.release();
-    fs1.release();
+    // use svd to compute the optimal transform
+    cv::Mat P1(0,3,CV_32FC1);
+    cv::Mat P2(0,3,CV_32FC1);
+    for (uint k=0; k<points1.size(); k++) {
+        if ((!points1[k].empty()) && (!points2[k].empty())) {
+            cv::vconcat(P1,cv::Mat(points1[k].size(),3,CV_32FC1,points1[k].data()),P1);
+            cv::vconcat(P2,cv::Mat(points2[k].size(),3,CV_32FC1,points2[k].data()),P2);
+            for (uint j=0; j<points1[k].size(); j++) {
+                outputFile1 << points1[k][j].x << '\t' << points1[k][j].y << '\t' << points1[k][j].z << std::endl;
+                outputFile2 << points2[k][j].x << '\t' << points2[k][j].y << '\t' << points2[k][j].z << std::endl;
+            }
+        } else {
+            std::cout << "image " << k << " valid" << std::endl;
+        }
+    }
+    P1 = P1.t();
+    P2 = P2.t();
 
-    // Stereo calibration
-    // t2 = R12 * t1 + t12
-    // R2 = R12 * R1
-    cv::Mat R12, t12, T12, F, E, t2;
-    cv::stereoCalibrate(cal1.object_points(), cal1.image_points(), cal2.image_points(), K1, D1, K2, D2, cal1.image_size(), R12, t12, F, E);
-    std::cout << R12 << std::endl << t12 << std::endl;
-    cv::hconcat(R12, t12, T12);
-    cv::hconcat(cv::Mat::zeros(1, 3, CV_64F), cv::Mat::ones(1, 1, CV_64F), t2);
-    cv::vconcat(T12, t2, T12);
+    // compute centroids
+    cv::Mat centroid1, centroid2;
+    reduce(P1,centroid1,1,CV_REDUCE_AVG);
+    reduce(P2,centroid2,1,CV_REDUCE_AVG);
+
+    // shift point clouds w.r.t. centroids
+    P1 = P1 - repeat(centroid1,1,P1.cols);
+    P2 = P2 - repeat(centroid2,1,P2.cols);
+    
+    // compute svd of P1*P2'
+    cv::Mat H = P2*(P1.t());
+    cv::Mat U,S,Vt;
+    cv::SVD::compute(H,S,U,Vt);
+    
+    // compute translation and rotation
+    cv::Mat R, t;
+    R = (U*Vt).t();
+    if (cv::determinant(R) < 0) {
+        cv::Mat c = R.col(2)*(-1.0f);
+        c.copyTo(R.col(2));
+    }
+    t = -R*centroid2 + centroid1;
+
+    //std::cout << "Rotation:" << R << std::endl << "Translation:" << t << std::endl;
+
+    // compute reprojection error
+    float total_error = 0.0;
+    uint N = 0;
+
+    for (uint k=0; k<points1.size(); k++) {
+        if ((!points1[k].empty()) && (!points2[k].empty())) {
+            for (uint j=0; j<points1[k].size(); j++) {
+                cv::Mat p2t = R*cv::Mat(points2[k][j]) + t;
+                total_error += cv::norm(points1[k][j] - cv::Point3f(p2t));
+                N++;
+            }
+        }
+    }
+
+    total_error = total_error/N;
+    std::cout << "<translation error>: " << total_error << "[m]" << std::endl;
+
+    // Construct transformation matrix
+    cv::Mat h1, h2, T21;
+    cv::hconcat(R, t, h1);
+    cv::hconcat(cv::Mat::zeros(1, 3, CV_32FC1), cv::Mat::ones(1, 1, CV_32FC1), h2);
+    cv::vconcat(h1, h2, T21);
+    T21.convertTo(T21, CV_64F); //
 
     // Load the extrinsic matrices of the already intrinsic device
     cv::Mat T10;
-    fs1 = cv::FileStorage(config_path1, cv::FileStorage::READ);
-    fs1["external_transformation"] >> T10;
-    fs1.release();
+    cv::FileStorage fs = cv::FileStorage(config_path1, cv::FileStorage::READ);
+    fs["camera"]["external_transformation"] >> T10;
+    fs.release();
+    
 
-    // We need to calculate R20 and T20
-    // So that we can calculate T0 and R0 when we measure T2 and R2
-    // t0 = R20 * t2 + t20
-    // R0 = R20 * R2
-    // i.e. T0 = T20*T2
-    // ruben doesn't get this??
-    T12.convertTo(T12, CV_64F);
-    T12.convertTo(T10, CV_64F);
-    cv::Mat T20 = T10*T12.inv();
-    std::cout << T20 << std::endl;
+    cv::Mat T20 = T10*T21;
+    std::cout << "Total external transformation matrix" << std::endl << T20 << std::endl;
     std::map<std::string, cv::Mat> mat_map({{"external_transformation", T20}});
-    Calibrator::dump_matrices(config_path2, mat_map);
-    Calibrator::set_integrated(config_path2, true);
+    dump_matrices(config_path2, mat_map);
+    set_integrated(config_path2, true);
 }
