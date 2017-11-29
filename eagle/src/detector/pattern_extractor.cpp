b0a6e67dd2e9886b26279668c79c67e9b04c5564
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2017-11-29 17:14:06 +0100

    pattern extraction with display added

--- eagle/src/detector/pattern_extractor.cpp
+++ eagle/src/detector/pattern_extractor.cpp
@@ -14,22 +14,31 @@ PatternExtractor::PatternExtractor(const Pattern& pattern) :
 
 cloud2_t PatternExtractor::extract(cv::Mat& img) {
     int t;
-    return _pattern.find(img, t, false);
+    cv::Point2f offset(0.0,0.0);
+    return extract(img, offset, t, false);
 }
 
-cloud2_t PatternExtractor::extract(cv::Mat& img, int& id) {
-    return _pattern.find(img, id, false);
+cloud2_t PatternExtractor::extract(cv::Mat& img, int& id, bool display)
+{
+    cv::Point2f offset(0.0,0.0);
+    return extract(img, offset, id, display);
 }
 
-cloud2_t PatternExtractor::extract(cv::Mat& img, const cv::Point2f& offset) {
+cloud2_t PatternExtractor::extract(cv::Mat& img, const cv::Point2f& offset, bool display)
+{
     int t;
-    return extract(img, offset, t);
+    return extract(img, offset, t, display);
 }
 
 cloud2_t PatternExtractor::extract(cv::Mat& img, const cv::Point2f& offset, int& id, bool display) {
     cloud2_t points = _pattern.find(img, id, display);
     for (uint k = 0; k < points.size(); k++) {
         points[k] += offset;
+    }
+
+    if (display) {
+        cv::imshow("Display",img);
+        cv::waitKey(500);
     }
 
     return points;
@@ -40,14 +49,15 @@ std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list) {
     return extract(list, t);
 }
 
-std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, std::vector<int>& id) {
+std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, std::vector<int>& id, bool display)
+{
     std::vector<cloud2_t> patterns;
     patterns.reserve(list.size());
     id.clear(); id.reserve(list.size());
     int tid;
 
-    for (uint k = 0; k < list.size(); k++) {
-        cloud2_t temp = extract(list[k], tid);
+    for (uint k=0; k<list.size(); k++) {
+        cloud2_t temp = extract(list[k], tid, display);
         if (!(_skip_invalid && temp.empty())) {
             patterns.push_back(temp);
             id.push_back(tid);
@@ -60,9 +70,10 @@ std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, std:
 
 }
 
-std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset) {
+std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset, bool display)
+{
     std::vector<int> t;
-    return extract(list, offset, t);
+    return extract(list, offset, t, display);
 }
 
 std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset, std::vector<int>& id, bool display) {
@@ -84,12 +95,13 @@ std::vector<cloud2_t> PatternExtractor::extract(std::vector<cv::Mat>& list, cons
     return patterns;
 }
 
-std::vector<cloud2_t> PatternExtractor::extract(const cv::String& path) {
+std::vector<cloud2_t> PatternExtractor::extract(const cv::String& path, bool display)
+{
     std::vector<int> t;
-    return extract(path, t);
+    return extract(path, t, display);
 }
 
-std::vector<cloud2_t> PatternExtractor::extract(const cv::String& path, std::vector<int>& id) {
+std::vector<cloud2_t> PatternExtractor::extract(const cv::String& path, std::vector<int>& id, bool display) {
     std::vector<cv::String> list;
     std::vector<cv::Mat> images;
     cv::Mat img;
@@ -102,5 +114,5 @@ std::vector<cloud2_t> PatternExtractor::extract(const cv::String& path, std::vec
         }
     }
 
-    return extract(images, id);
+    return extract(images, id, display);
 }
--- eagle/src/detector/pattern_extractor.h
+++ eagle/src/detector/pattern_extractor.h
@@ -7,34 +7,35 @@
 
 namespace eagle {
 
-typedef std::vector<cv::Point2f> cloud2_t;
+    typedef std::vector<cv::Point2f> cloud2_t;
 
-class PatternExtractor {
-  private:
-    Pattern _pattern;
-    bool _skip_invalid = true;
+    class PatternExtractor
+    {
+        private:
+            Pattern _pattern;
+            bool _skip_invalid = true;
+    
+        public:
+            PatternExtractor(const cv::String& config);
+            PatternExtractor(const Pattern& pattern);
+    
+            cloud2_t extract(cv::Mat& img);
+            cloud2_t extract(cv::Mat& img, int& id, bool display = false);
+            cloud2_t extract(cv::Mat& img, const cv::Point2f& offset, bool display = false);
+            cloud2_t extract(cv::Mat& img, const cv::Point2f& offset, int& id, bool display = false);
 
-  public:
-    PatternExtractor(const cv::String& config);
-    PatternExtractor(const Pattern& pattern);
+            std::vector<cloud2_t> extract(std::vector<cv::Mat>& list);
+            std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, std::vector<int>& id, bool display = false);
+            std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset, bool display = false);
+            std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset, std::vector<int>& id, bool display = false);
 
-    cloud2_t extract(cv::Mat& img);
-    cloud2_t extract(cv::Mat& img, int& id);
-    cloud2_t extract(cv::Mat& img, const cv::Point2f& offset);
-    cloud2_t extract(cv::Mat& img, const cv::Point2f& offset, int& id, bool display = false);
-
-    std::vector<cloud2_t> extract(std::vector<cv::Mat>& list);
-    std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, std::vector<int>& id);
-    std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset);
-    std::vector<cloud2_t> extract(std::vector<cv::Mat>& list, const std::vector<cv::Point2f>& offset, std::vector<int>& id, bool display = false);
-
-    std::vector<cloud2_t> extract(const cv::String& path);
-    std::vector<cloud2_t> extract(const cv::String& path, std::vector<int>& id);
-
-    Pattern pattern() { return _pattern; }
-    void set_skip_invalid(bool skip = true) { _skip_invalid = skip; }
-    bool skip_invalid() { return _skip_invalid; }
-};
+            std::vector<cloud2_t> extract(const cv::String& path, bool display = false);
+            std::vector<cloud2_t> extract(const cv::String& path, std::vector<int>& id, bool display = false);
+    
+            Pattern pattern() { return _pattern; }
+            void set_skip_invalid(bool skip = true) { _skip_invalid = skip; }
+            bool skip_invalid() { return _skip_invalid; }
+    };
 };
 
 #endif //PATTERN_EXTRACTOR_H
